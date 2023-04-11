54d35a7a54f642100ecfe3ab63c1aa2b2483ce1c
lehoanganh1998 <boiduongtoan2012@gmail.com>
2023-04-11 17:36:00 +0800

    Bum Chiu

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-import blockcypher
 import datetime
 import pandas as pd
 import time
+import requests
 from tqdm import tqdm
 import os
 
-#! BATCH NUMBER
+# BATCH NUMBER
 batch_num = 1
 # data import
-dataset_folder = "/home/lee/STL project/dataset"
+dataset_folder = "./dataset"
 folder_list = os.listdir(dataset_folder)
 
 # dataset list create
@@ -19,29 +19,54 @@ for file in folder_list:
         addresses_df = pd.read_csv(dataset_loc)
         btc_addresses = addresses_df['account'].tolist()
 
-delay_seconds = 25  # Delay between requests in seconds for the limit
+delay_seconds = 10  # Delay between requests in seconds for the limit
 features = []
 
+def fetch_address_data(address):
+    url = f"https://blockstream.info/api/address/{address}/txs"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError(f"Unable to fetch data for address {address}")
+
 for address in tqdm(btc_addresses):
-    data = blockcypher.get_address_details(address,api_key="a8af2661a5e34021a064aee76e3790f4")
-    first_seen = data['txrefs'][0]['confirmed']
-    last_seen = data['txrefs'][-1]['confirmed']
+    data = fetch_address_data(address)
+    first_seen = datetime.datetime.fromtimestamp(data[0]['status']['block_time'])
+    last_seen = datetime.datetime.fromtimestamp(data[-1]['status']['block_time'])
     lifetime = abs((last_seen - first_seen).days)
-    active_days = len(set([txref['confirmed'].strftime('%Y-%m-%d') for txref in data['txrefs']]))
-    incoming_amount = sum([txref['value'] for txref in data['txrefs'] if txref['tx_input_n'] == -1])
-    outgoing_amount = sum([txref['value'] for txref in data['txrefs'] if txref['tx_output_n'] == -1])
-    incoming_tx_count = sum([1 for txref in data['txrefs'] if txref['tx_input_n'] == -1])
-    outgoing_tx_count = sum([1 for txref in data['txrefs'] if txref['tx_output_n'] == -1])
+    active_days = len(set([datetime.datetime.fromtimestamp(tx['status']['block_time']).strftime('%Y-%m-%d') for tx in data]))
+
+    incoming_amount = 0
+    outgoing_amount = 0
+    incoming_tx_count = 0
+    outgoing_tx_count = 0
+
+    for tx in data:
+        is_incoming_tx = False
+        is_outgoing_tx = False
+
+        for vin in tx['vin']:
+            if vin['prevout']['scriptpubkey_address'] == address:
+                outgoing_amount += vin['prevout']['value']
+                is_outgoing_tx = True
+
+        for vout in tx['vout']:
+            if vout['scriptpubkey_address'] == address:
+                incoming_amount += vout['value']
+                is_incoming_tx = True
+
+        incoming_tx_count += is_incoming_tx
+        outgoing_tx_count += is_outgoing_tx
 
     features.append((address, lifetime, active_days, incoming_amount, outgoing_amount, incoming_tx_count, outgoing_tx_count))
-    
     time.sleep(delay_seconds)  # Add delay between requests
 
 df = pd.DataFrame(features, columns=['address', 'lifetime', 'active_days', 'incoming_amount', 'outgoing_amount', 'incoming_tx_count', 'outgoing_tx_count'])
 
 # Save the DataFrame to a CSV file
-output_folder = "/home/lee/STL project/result"
-output_name = "result_batch_" + batch_num
-output_file = os.path.join(output_folder,output_name)
+output_folder = "./result"
+os.makedirs(output_folder, exist_ok=True)
+output_name = "result_batch_{}".format(batch_num)
+output_file = os.path.join(output_folder, output_name)
 df.to_csv(output_file, index=False)
-
